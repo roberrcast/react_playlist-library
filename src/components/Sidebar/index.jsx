e7fcada0e8f8def93b7384256e12f0210588cfd7
robercast <dunamis1.16@gmail.com>
2025-09-17 02:29:10 -0700

    added style to sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { FaSearch } from "react-icons/fa";
-//import { songList } from "../../songInfo.js";
-//import Song from "../Song";
 import List from "../List";
 import "./Sidebar.scss";
 
@@ -10,24 +8,26 @@ class Sidebar extends Component {
         return (
             <>
                 <aside className="sidebar">
-                    <search className="sidebar__search">
-                        <form action="" className="sidebar__form">
-                            <input
-                                type="search"
-                                id="search"
-                                className="sidebar__input"
-                                placeholder="Búsqueda"
-                            />
-                            <button className="sidebar__button">
-                                <FaSearch />
-                            </button>
-                        </form>
-                    </search>
+                    <div className="sidebar__wrapper">
+                        <search className="sidebar__search">
+                            <form action="" className="sidebar__form">
+                                <input
+                                    type="search"
+                                    id="search"
+                                    className="sidebar__input"
+                                    placeholder="Búsqueda"
+                                />
+                                <button className="sidebar__button">
+                                    <FaSearch />
+                                </button>
+                            </form>
+                        </search>
 
-                    <section className="sidebar__list">
-                        <h2 className="sidebar__title">Playlists</h2>
-                        <List name="Playlist 1" />
-                    </section>
+                        <section className="sidebar__list">
+                            <h2 className="sidebar__title">Playlists</h2>
+                            <List name="Playlist 1" />
+                        </section>
+                    </div>
                 </aside>
             </>
         );

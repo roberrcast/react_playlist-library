b3a9c381c8e1d23493130e727362ed70d4231ebd
robercast <dunamis1.16@gmail.com>
2025-12-28 05:28:54 -0800

    saving progress

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
+import "./Breadcrumb.scss";
 
 const Breadcrumb = () => {
     const [searchParams, setSearchParams] = useSearchParams();

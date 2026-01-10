919e25291d7868e0adfdc79c4e532f5e59ac286f
robercast <dunamis1.16@gmail.com>
2026-01-09 16:25:05 -0800

    modified files and add new file

---
+++ src/theme/index.js
@@ -0,0 +1,28 @@
+const Theme = {
+    colors: {
+        background: "#1e1e1e",
+        inputBg: "hsla(0, 0%, 15%, 0.75)",
+        coolBlue: "hsla(217.4, 89%, 60.8%, 0.75)",
+        sidebarBg: "hsla(0, 0%, 7%, 0.75)",
+        primaryFont: "#fafafa",
+        icon: "#a9a9a9",
+        tick: "#4f6272",
+        searchIcon: "hsla(0, 0%, 29.8%, 1)",
+        dryRed: "#6b312d",
+        redHover: "#bb453a",
+        searchBarFocus: "#a6e2a2",
+    },
+
+    font: {
+        primary: '"Roboto", Arial, sans-serif',
+        weights: {
+            global: 100,
+        },
+    },
+
+    spacing: {
+        defaultPadding: "0 25px",
+    },
+};
+
+export default Theme;
